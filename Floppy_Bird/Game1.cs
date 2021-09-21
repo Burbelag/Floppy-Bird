68b09f18a443d97898a3a545ee991d8001a1c2c7
Burbelag <duduzovmisa@gmail.com>
2021-09-22 00:14:23 +0300

    yes.

---
+++ Floppy_Bird/Game1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Floppy_Bird
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphicsDeviceManager;
+        
+        private SpriteBatch _spriteBatch;
+        private Texture2D _floppy;
+        private Texture2D _background;
+        private Texture2D _floppyDriver;
+        
+        private Vector2 _floppyPos = new Vector2(200.0f, 2.0f);
+        private Vector2 _floppyDriverDownPos = new Vector2(300, 0f);
+        private Vector2 _floppyDriverUpPos = new Vector2(300, 0f);
+        private Vector2 _driverDownPos0;
+        private Vector2 _driverUpPos0;
+        
+        private Vector3 _gameCamera;
+
+        private KeyboardState _oldKeyboardState;
+
+        private List<Vector2> _listUpperDriver = new();
+        private List<Vector2> _listDownDriver = new();
+
+        private float _floppyScale;
+        private float _velocity = 0;
+        private float _acceleration = 0.15f;
+        private  int _pipeBetweenPosition = 0;
+        private double _screenResolution;
+        private float _backgroundScale;
+        private float _floppyDriverScale;
+        private float _cameraPos = 0;
+        private bool _menu = true;
+
+        private const float DefaultXSpeed = 0.8f;
+        public Game1()
+        {
+            IsMouseVisible = false;
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _floppy = Content.Load<Texture2D>("floppy");
+            _background = Content.Load<Texture2D>("background");
+            _floppyDriver = Content.Load<Texture2D>("floppy_driver");
+
+            //нахождение как скалировать через теоремы пифагора
+
+            _screenResolution = _graphicsDeviceManager.PreferredBackBufferHeight ^
+                                2 / _graphicsDeviceManager.PreferredBackBufferWidth ^ 2;
+            Math.Sqrt(_screenResolution);
+
+            _backgroundScale = _background.Height ^ 2 / _background.Width ^ 2;
+            Math.Sqrt(_backgroundScale);
+
+            //end
+
+            _backgroundScale = (float) (_screenResolution / _backgroundScale);
+
+            //Скейл драйвера в 2 раза меньше, вроде нормально
+
+            _floppyDriverScale = _backgroundScale / 2;
+            _floppyScale = (float) (_backgroundScale * 1.5);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            KeyboardState newState = Keyboard.GetState();
+
+            if (_menu)
+            {
+                KeyboardState state = Keyboard.GetState();
+
+                if (state.IsKeyDown(Keys.Space)) _menu = false;
+
+            }
+            else
+            {
+                if (_oldKeyboardState.IsKeyUp(Keys.Space) && newState.IsKeyDown(Keys.Space))
+                {
+                    _velocity = -5f;
+                }
+
+                /*  COLLISION STUFF */
+                Rectangle floppy = new Rectangle((int) _floppyPos.X, (int) _floppyPos.Y, _floppy.Width, _floppy.Height);
+                Rectangle drive = new Rectangle((int) _driverDownPos0.X, (int) _driverDownPos0.Y,
+                    (int) (_floppyDriver.Width * _floppyDriverScale),
+                    (int) (_floppyDriver.Height * _floppyDriverScale));
+                Rectangle driveUp = new Rectangle((int) _driverUpPos0.X, (int) _driverUpPos0.Y,
+                    (int) (_floppyDriver.Width * _floppyDriverScale),
+                    (int) (_floppyDriver.Height * _floppyDriverScale));
+
+                if (Collision(floppy, drive) || Collision(floppy, driveUp))
+                    Console.WriteLine("AAAAAAAAA");
+
+                if (floppy.X < 0 || floppy.X > _graphicsDeviceManager.PreferredBackBufferWidth ||
+                    floppy.Y < 0 || floppy.Y > _graphicsDeviceManager.PreferredBackBufferHeight)
+                    Console.WriteLine("HUY");
+
+                if (_pipeBetweenPosition > 200)
+                {
+                    _listUpperDriver.Add(Draw_pipe(_floppyDriverDownPos.X += _pipeBetweenPosition));
+                    _listUpperDriver.Add(Draw_pipe(_floppyDriverUpPos.X += _pipeBetweenPosition, 
+                        _graphicsDeviceManager.PreferredBackBufferHeight));
+                    _pipeBetweenPosition = 0;
+                }
+                _pipeBetweenPosition += 1;
+
+                _driverDownPos0 = new Vector2(200,
+                    _graphicsDeviceManager.PreferredBackBufferHeight - _floppyDriver.Height * _floppyDriverScale);
+                _driverUpPos0 = new Vector2(200, 0);
+
+                _oldKeyboardState = newState;
+                _velocity += _acceleration;
+                _floppyPos.Y += _velocity;
+                _floppyPos.X += DefaultXSpeed;
+        
+                
+                _gameCamera = new Vector3(_cameraPos -=DefaultXSpeed, 0, 0.0f);
+
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Purple);
+
+            //draw static background
+            _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp);
+
+            _spriteBatch.Draw(_background, Vector2.Zero, null, Color.White, 0.0f,
+                Vector2.Zero, _backgroundScale, SpriteEffects.None, 0.0f);
+
+            _spriteBatch.End();
+
+            if (_menu)
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(_floppy,new Rectangle(_graphicsDeviceManager.PreferredBackBufferWidth / 2, 
+                    _graphicsDeviceManager.PreferredBackBufferHeight / 2, _floppy.Width, _floppy.Height), Color.White);
+                _spriteBatch.End();
+            }
+            else
+            {
+                _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp,
+                    null, null, null, Matrix.CreateTranslation(_gameCamera));
+               
+                _spriteBatch.Draw(_floppy, _floppyPos,
+                    null, Color.White, 0.0f,
+                    Vector2.Zero, _floppyScale, SpriteEffects.None, 0.0f);
+
+/*
+                _spriteBatch.Draw(_floppy_driver, _driverDownPos0,
+                    null, Color.White, 0.0f,
+                    Vector2.Zero, _floppyDriverScale, SpriteEffects.None, 0.0f);
+
+                _spriteBatch.Draw(_floppy_driver, driverRect, Color.White);
+                _spriteBatch.Draw(_floppy_driver, _driverUpPos0,
+                    null, Color.White, 0.0f,
+                    Vector2.Zero, _floppyDriverScale, SpriteEffects.None, 0.0f);
+*/
+                foreach (var drawPipes in _listUpperDriver)
+                {
+                    _spriteBatch.Draw(_floppyDriver, new Rectangle((int) drawPipes.X, (int) drawPipes.Y, 
+                            (int) (_floppyDriver.Width*_floppyDriverScale), (int) (_floppyDriver.Height*_floppyDriverScale)), 
+                        Color.White);
+                }
+                _spriteBatch.End();
+            }
+            base.Draw(gameTime);
+        }
+
+        private Vector2 Draw_pipe(float driverPosX)
+        {
+            driverPosX += _graphicsDeviceManager.PreferredBackBufferWidth;
+            return new Vector2(driverPosX, 0);
+        }
+
+        private Vector2 Draw_pipe(float driverPosX, float driverPosY)
+        {
+            driverPosX += _graphicsDeviceManager.PreferredBackBufferWidth;
+            driverPosY += _graphicsDeviceManager.PreferredBackBufferHeight;
+
+            return new Vector2(driverPosX, driverPosY);
+        }
+        
+
+        private static bool Collision(Rectangle a, Rectangle b)
+        {
+            return a.X < b.X + b.Width &&
+                   a.X + a.Width > b.X &&
+                   a.Y < b.Y + b.Height &&
+                   a.Y + a.Height > b.Y;
+        }
+
+    }
+}
